c7c2eccc68ec708d64b9af6823a3c52aab48a58c
Nils Bartels <nils.bartels@clt106153.collins.kg>
2014-05-12 14:11:06 +0200

    [ADD] basic api stuff and some keys on models;

--- api/v1/api.rb
+++ api/v1/api.rb
@@ -8,6 +8,7 @@ require './model/srv.rb'
 
 configure do
   MongoMapper.setup({ 'production' => { 'uri' => CONFIG['mongodb']['uri'] } }, 'production')
+  MongoMapper.database = 'srvinv'
 end
 
 get '/' do
@@ -15,20 +16,32 @@ get '/' do
 end
 
 get '/envs' do
+  @result = Env.sort(:created_at.desc)
 end
 
-get '/envs/*' do
-  params[:splat]
+get '/envs/:name' do
+  @result = Env.where(:name=>params['name']).first
+  return status 404 if @result.nil?
 end
 
 post '/envs' do
+  # we need to validate if it's already there
+  @env = Env.new(params['env'])
+  @env.save
   status 201
 end
 
 put '/envs/:name' do
+  @env = Env.where(:name=>params['name']).first
+  return status 404 if @env.nil?
+  @env.update(params['name'])
+  @env.save
   status 202
 end
 
 delete '/envs/:name' do
+  @env = Env.where(:name=>params['name']).first
+  return status 404 if @env.nil?
+  @env.delete
   status 202
 end
--- model/srv.rb
+++ model/srv.rb
@@ -3,14 +3,16 @@
 class Srv
   include MongoMapper::Document
 
-  key :name,            String
-  key :description,     String
-  key :created_at,      Time
+  key :name,              String
+  key :description,       String
+  key :created_at,        Time
 
-  key :cpu_cores,       Integer, default: 1
-  key :ram_size_mb,     Integer, default: 1024
-  key :disk_size_gb,    Integer, default: 40
-  key :services,        Array
+  key :cpu_cores,         Integer, default: 1
+  key :ram_size_mb,       Integer, default: 1024
+  key :disk_size_gb,      Integer, default: 40
+  key :services,          Array
+  key :is_provisioned,    Boolean, default: false
+  key :provisioned_with,  String
 
   belongs_to :net
 
